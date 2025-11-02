3ad4b9080be575d3c31bafa5ecfe82224caa07e7
abisoyeonanuga <102636953+dottymatrix@users.noreply.github.com>
2025-11-02 18:45:21 -0500

    update postcss, tailwin  config, global css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./src/app/**/*.{js,ts,jsx,tsx}",
-        "./src/components/**/*.{js,ts,jsx,tsx}"
+      './src/app/**/*.{js,ts,jsx,tsx}',
+      './src/components/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
-        extend: {
-            fontFamily: {
-                title: ['var(--font-title)', 'sans-serif'],
-                body: ['var(--font-body)', 'sans-serif'],
-            },
-        }
-        ,
+      extend: {
+        fontFamily: {
+          sans: ['var(--font-ubuntu)', 'sans-serif'],
+          title: ['Anton', 'sans-serif'],
+          body: ['Inter', 'sans-serif'],
+        },
+        colors: {
+          bg: '#333',
+          content: '#222',
+          fg: '#bcbcbc',
+          bright: '#ddd',
+          nodevember: '#bc1010',
+        },
+      },
     },
     plugins: [],
 }
-  
