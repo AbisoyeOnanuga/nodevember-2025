2e171a4e0c18221710e13b425443c50c2c2ef6ab
abisoyeonanuga <102636953+dottymatrix@users.noreply.github.com>
2025-11-04 07:59:49 -0500

    update package, head compnent

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
       <>
         <title>Unofficial Nodevember 2025</title>
         <meta name="description" content="A community-curated prompt list and creative tracker for Nodevember 2025." />
-        <link rel="icon" href="/public/favicon.png" />
+        <link rel="icon" href="/favicon.png" />
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/theme-toggles@4.10.1/css/classic.min.css"
